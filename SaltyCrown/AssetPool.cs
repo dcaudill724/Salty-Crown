1b597311d8506593b0d465a4a60279dd85b2e1cc
Noah Tranbarger <noahtranbarger@gmail.com>
2017-09-07 18:03:21 -0400

    Added some stuff to AssetPool.cs

--- SaltyCrown/AssetPool.cs
+++ SaltyCrown/AssetPool.cs
@@ -1,5 +1,17 @@
-﻿namespace SaltyCrown {
-    public static class AssetPool {
+﻿using System.IO;
 
+namespace SaltyCrown {
+    public static class AssetPool {
+        public static void ParseAssets(string path) {
+            if (!File.Exists(path)) {
+                System.Console.WriteLine("File does not exist");
+                return;
+            }
+
+            string[] lines = path.Split('\n');
+            for (int i = 0; i < lines.Length; i++) {
+                string[] items = lines[i].Split(' ');
+            }
+        }
     }
 }
