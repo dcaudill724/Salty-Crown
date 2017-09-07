996763adb86de06f65123e9de0d8e8a653444131
Noah Tranbarger <noahtranbarger@gmail.com>
2017-09-06 23:26:59 -0400

    Intial commit.

---
+++ SaltyCrown/Program.cs
@@ -0,0 +1,18 @@
+﻿//Authors:
+//Noah Tranbarger
+//Dustin Caudill
+
+using System;
+
+namespace SaltyCrown {
+#if WINDOWS || LINUX
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using (var game = new App()) {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
