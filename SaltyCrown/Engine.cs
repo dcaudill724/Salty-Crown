b128d095d1ee755b098a61b564d795cd514c58d7
Dustin Caudill <dustincaudill724@gmail.com>
2017-09-11 13:32:01 -0400

    Had dumb stuff in Engine.cs but deleted it

--- SaltyCrown/Engine.cs
+++ SaltyCrown/Engine.cs
@@ -13,13 +13,10 @@ namespace SaltyCrown {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         AssetPool assetPool = new AssetPool();
-        delegate void x(int y);
 
         public Engine() {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            x help = i => {Console.WriteLine(i);};
-            help(20);
         }
         
         protected override void Initialize() {
